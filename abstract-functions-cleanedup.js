d11c37c44a35b059286682060e5a902dd97e2372
TimHyatt707 <timhyatt707@gmail.com>
2017-07-12 07:55:46 -0700

    cleaned up code

---
+++ abstract-functions-cleanedup.js
@@ -0,0 +1,12 @@
+// Clean up the code below with a single function called `personalIntroduction`
+// The new code should have the exact same functionality and output.
+
+function personalIntroduction(name,height,favoriteColor){
+  return console.log("My name " + name + ", and my height is" + height +
+  ", and my favorite color is " + favoriteColor);
+}
+
+personalIntroduction("Bob", "72", "red");
+personalIntroduction("Samantha",'65',"orange");
+personalIntroduction("Christopher",'69',"green");
+personalIntroduction("Tamera",'63',"blue");
